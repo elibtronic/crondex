917f77863259833f196fcad11f469c3df023b8a2
Tim Ribaric <tim.ribaric@gmail.com>
2015-01-07 09:21:18 -0500

    leading 0's
    leading 0 b/c dammit.

--- mday.py
+++ mday.py
@@ -9,14 +9,26 @@ from os import path
 from settings import *
 
 def make_today():
+
+    if len(str(date.today().month)) == 1:
+        t_month = "0" + str(date.today().month)
+    else:
+        t_month = str(date.today().month)
+
+    if len(str(date.today().day)) == 1:
+        t_day = "0" + str(date.today().day)
+    else:
+        t_day = str(date.today().day)
+
+    
     print "creating...",
-    fname = str(date.today().month) + "_"+ str(date.today().day)+".txt"
+    fname = t_month + "_" + t_day +".txt"
     if path.exists(fname):
         print "file exist"
         return False
     else:
         f = open(fname, "w")
-        f.write("#\n" + str(date.today().month) + "\n" + str(date.today().day) + "\n#\n\n") 
+        f.write("#\n" + t_month + "\n" + t_day + "\n#\n\n") 
         f.write("## Today ##\n\n")
         f.write("## ETC ##\n\n")
         f.write("## Stuff ##\n\n")
